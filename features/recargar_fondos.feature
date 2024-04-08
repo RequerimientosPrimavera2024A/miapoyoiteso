20f477a2418d383caf10ad0e9683994c7fb89d8d
Yael <yael.rodriguez@iteso.mx>
2024-04-08 08:38:02 -0600

    Finiquitado

--- features/recargar_fondos.feature
+++ features/recargar_fondos.feature
@@ -2,63 +2,59 @@ Feature: Como un usuario registrado quiero poder recargar fondos en mi cartera p
     Requerimiento para recargar fondos
 
 Example: En el que se accede al perfil para recargar fondos
-    Given: Estoy en el perfil de la cuenta
-    When: Intento recargar
-    Then:
+    Given Estoy en el perfil de la cuenta
+    When Intento recargar fondos y no hay errores externos
+    Then Los fondos se añadirán a la cuenta
 
 Example: En el que se trata de recargar fondos desde un sitio distinto al perfil
-    Given: No estoy en el perfil de la cuenta
-    When:
-    Then:
+    Given No estoy en el perfil de la cuenta
+    When Busco la manera de recargar fondos
+    Then No encuentro forma de recargarlos desde el sitio actual
 
 Example: En el que se recarga mediante tarjeta de crédito
-    Given:
-    When:
-    Then:
+    Given Se intenta hacer una recarga
+    When Se selecciona la opción por tarjeta
+    Then Se ingresan los datos necesarios
 
 Example: En el que se recarga mediante una trasferencia bancaria
-    Given:
-    When:
-    Then:
+    Given Se intenta hacer una recarga
+    When Se selecciona la opción por trasnferencia
+    Then Se espera el envío de los fondos
 
 Example: En el que se busca recargar por un medio no soportado por la plataforma
-    Given:
-    When:
-    Then:
+    Given Se intenta hacer una recarga
+    When El método necesitado no está disponible en la plataforma
+    Then No hay manera de añadir fondos por este medio
 
 Example: En el que el usuario entiende todos los pasos a seguir para la recarga
-    Given:
-    When:
-    Then:
+    Given El usuario intenta realizar alguna recarga
+    When Sigue los pasos de forma correcta
+    Then El usuario puede terminar el proceso sin problemas
 
 Example: En el que el usuario se atora en ciertas etapas del proceso
-    Given:
-    When:
-    Then:
+    Given El usuario intenta realizar alguna recarga
+    When Se atorá o no entiende cuales son los pasos a seguir
+    Then Se le ofrece guía por medio de preguntas frecuentes
 
 Example: En el que la transacción no es exitosa y falla la actualización
-    Given:
-    When:
-    Then:
+    Given Se intenta hacer una recarga
+    When La transacción no es exitosa
+    Then Se notifica de la falla y no cambian los fondos
 
 Example: En el que la transacción es exitosa y se actualizan los fondos
-    Given:
-    When:
-    Then:
+    Given Se intenta hacer una recarga
+    When La transacción es exitosa
+    Then Se actualizan los fondos de la cuenta
 
 Example: En el que se envía la notificación mediante la aplicación
-    Given:
-    When:
-    Then:
+    Given Se tiene la aplicación y las configuraciones permiten notificaciones
+    When Se intenta notificar de una acción
+    Then Llega al dispositivo con la aplicación una notificación
 
 Example: En el que se envía la notificación por medios externos
-    Given:
-    When:
-    Then:
+    Given Las configuraciones permiten notificaciones externas
+    When Se intenta notificar de una acción
+    Then Se manda notificación por el medio externo seleccionado
 
-Example: En el que no hay sitio por donde notificar al usuario
-    Given:
-    When:
-    Then:
 
 
---
+++ features/step_definitions/recargar_fondos.js
@@ -0,0 +1,138 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('Estoy en el perfil de la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Intento recargar fondos y no hay errores externos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Los fondos se añadirán a la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('No estoy en el perfil de la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Busco la manera de recargar fondos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('No encuentro forma de recargarlos desde el sitio actual', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('Se intenta hacer una recarga', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Se selecciona la opción por tarjeta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se ingresan los datos necesarios', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Se selecciona la opción por trasnferencia', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se espera el envío de los fondos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('El método necesitado no está disponible en la plataforma', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('No hay manera de añadir fondos por este medio', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  When('Sigue los pasos de forma correcta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('El usuario puede terminar el proceso sin problemas', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('El usuario intenta realizar alguna recarga', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Se atorá o no entiende cuales son los pasos a seguir', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se le ofrece guía por medio de preguntas frecuentes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('La transacción no es exitosa', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se notifica de la falla y no cambian los fondos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('La transacción es exitosa', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se actualizan los fondos de la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('Se tiene la aplicación y las configuraciones permiten notificaciones', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Se intenta notificar de una acción', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Llega al dispositivo con la aplicación una notificación', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('Las configuraciones permiten notificaciones externas', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Se manda notificación por el medio externo seleccionado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
