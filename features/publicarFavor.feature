dfa2700dd09807b6750d3f13c70178e6679ecee6
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2024-04-08 08:42:02 -0600

    Actividad finalizada, no pude corregir el error

---
+++ features/publicarFavor.feature
@@ -0,0 +1,57 @@
+Feature: Como un usuario registrado, quiero poder publicar un favor en el muro para solicitar ayuda de la comunidad universitaria
+  La aplicación debe tener un feed con todas las publicaciones de favores
+
+  Scenario: En el que el usuario presiona el botón y crea un favor
+    Given estoy en el feed de la aplicación
+    When presiono el botón de crear favor
+    Then la aplicación me permite crear una publicación
+
+  Scenario: En el que el usuario no presiona el botón
+    Given estoy en el feed de la aplicación
+    When no presiono el botón de crear favor
+    Then sigo viendo el feed de publicaciones
+
+  Scenario: En el que se llena el formulario correctamente
+    Given estoy en el formulario para publicar un favor
+    When lleno el formulario correctamente
+    Then la aplicación me permite publicar el favor
+
+  Scenario: En el que no se escribe una descripción
+    Given estoy en el formulario para publicar un favor
+    When no escribo una descripción para el favor
+    Then la aplicación no me permite publicar el favor
+
+  Scenario: En el que no se selecciona una categoría
+    Given estoy en el formulario para publicar un favor
+    When no selecciono una categoría
+    Then la aplicación no me permite publicar el favor
+
+  Scenario: En el que seleccion varias categorías
+    Given estoy en el formulario para publicar un favor
+    When selecciono varias categorías para el favor
+    Then la aplicación no me permite publicar el favor
+
+  Scenario: En el que se muestra la publicación
+    Given estoy en el feed de la aplicación
+    When llené el formulario correctamente
+    Then mi favor se publica en el feed de la aplicación
+
+  Scenario: En el que el favor publicado es el más reciente
+    Given estoy en el feed de la aplicación
+    When publico mi favor en la aplicación y es la más reciente
+    Then mi publicación se muestra hasta arriba en el feed
+
+  Scenario: En el que el favor publicado no es el más reciente
+    Given estoy en el feed de la aplicación
+    When publiqué mi favor hace algún tiempo y no es la más reciente
+    Then mi publicación no es la primera en el feed y se va desplazando hacia abajo conforme se publican nuevos favores
+
+  Scenario: En el que el favor incumple con las normas de conducta
+    Given estoy en el feed de la aplicación
+    When publiqué un favor que incumple las normas de conducta
+    Then mi publicación es eliminada del feed
+
+  Scenario: En el que el favor cumple con las normas de conducta
+    Given estoy en el feed de la aplicación
+    When publiqué un favor que cumple con las normas de conducta
+    Then mi publicación se mantendrá en el feed
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,287 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+// 1) Scenario: En el que el usuario presiona el botón y crea un favor # features\publicarFavor.feature:4
+//    ? Given estoy en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When presiono el botón de crear favor
+//        Undefined. Implement with the following snippet:
+
+         When('presiono el botón de crear favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then la aplicación me permite crear una publicación
+//        Undefined. Implement with the following snippet:
+
+         Then('la aplicación me permite crear una publicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 2) Scenario: En el que el usuario no presiona el botón # features\publicarFavor.feature:9
+//    ? Given estoy en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When no presiono el botón de crear favor
+//        Undefined. Implement with the following snippet:
+
+         When('no presiono el botón de crear favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then sigo viendo el feed de publicaciones
+//        Undefined. Implement with the following snippet:
+
+         Then('sigo viendo el feed de publicaciones', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 3) Scenario: En el que se llena el formulario correctamente # features\publicarFavor.feature:14
+//    ? Given estoy en el formulario para publicar un favor
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el formulario para publicar un favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When lleno el formulario correctamente
+//        Undefined. Implement with the following snippet:
+
+         When('lleno el formulario correctamente', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then la aplicación me permite publicar el favor
+//        Undefined. Implement with the following snippet:
+
+         Then('la aplicación me permite publicar el favor', function () {    
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 4) Scenario: En el que no se escribe una descripción # features\publicarFavor.feature:19
+//    ? Given estoy en el formulario para publicar un favor
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el formulario para publicar un favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When no escribo una descripción para el favor
+//        Undefined. Implement with the following snippet:
+
+         When('no escribo una descripción para el favor', function () {      
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then la aplicación no me permite publicar el favor
+//        Undefined. Implement with the following snippet:
+
+         Then('la aplicación no me permite publicar el favor', function () { 
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 5) Scenario: En el que no se selecciona una categoría # features\publicarFavor.feature:24
+//    ? Given estoy en el formulario para publicar un favor
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el formulario para publicar un favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When no selecciono una categoría
+//        Undefined. Implement with the following snippet:
+
+         When('no selecciono una categoría', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then la aplicación no me permite publicar el favor
+//        Undefined. Implement with the following snippet:
+
+         Then('la aplicación no me permite publicar el favor', function () { 
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 6) Scenario: En el que seleccion varias categorías # features\publicarFavor.feature:29
+//    ? Given estoy en el formulario para publicar un favor
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el formulario para publicar un favor', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When selecciono varias categorías para el favor
+//        Undefined. Implement with the following snippet:
+
+         When('selecciono varias categorías para el favor', function () {    
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then la aplicación no me permite publicar el favor
+//        Undefined. Implement with the following snippet:
+
+         Then('la aplicación no me permite publicar el favor', function () { 
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 7) Scenario: En el que se muestra la publicación # features\publicarFavor.feature:34
+//    ? Given estoy en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When llené el formulario correctamente
+//        Undefined. Implement with the following snippet:
+
+         When('llené el formulario correctamente', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then mi favor se publica en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Then('mi favor se publica en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 8) Scenario: En el que el favor publicado es el más reciente # features\publicarFavor.feature:39
+//    ? Given estoy en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When publico mi favor en la aplicación y es la más reciente
+//        Undefined. Implement with the following snippet:
+
+         When('publico mi favor en la aplicación y es la más reciente', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then mi publicación se muestra hasta arriba en el feed
+//        Undefined. Implement with the following snippet:
+
+         Then('mi publicación se muestra hasta arriba en el feed', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 9) Scenario: En el que el favor publicado no es el más reciente # features\publicarFavor.feature:44
+//    ? Given estoy en el feed de la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('estoy en el feed de la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When publiqué mi favor hace algún tiempo y no es la más reciente        
+//        Undefined. Implement with the following snippet:
+
+         When('publiqué mi favor hace algún tiempo y no es la más reciente', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then mi publicación no es la primera en el feed y se va desplazando hacia abajo conforme se publican nuevos favores
+//        Undefined. Implement with the following snippet:
+
+         Then('mi publicación no es la primera en el feed y se va desplazando hacia abajo conforme se publican nuevos favores', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 10) Scenario: En el que el favor incumple con las normas de conducta # features\publicarFavor.feature:49
+//     ? Given estoy en el feed de la aplicación
+//         Undefined. Implement with the following snippet:
+
+          Given('estoy en el feed de la aplicación', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+
+    // ? When publiqué un favor que incumple las normas de conducta
+    //     Undefined. Implement with the following snippet:
+
+          When('publiqué un favor que incumple las normas de conducta', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+
+    // ? Then mi publicación es eliminada del feed
+    //     Undefined. Implement with the following snippet:
+
+          Then('mi publicación es eliminada del feed', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+
+
+// 11) Scenario: En el que el favor cumple con las normas de conducta # features\publicarFavor.feature:54
+//     ? Given estoy en el feed de la aplicación
+//         Undefined. Implement with the following snippet:
+
+          Given('estoy en el feed de la aplicación', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+
+    // ? When publiqué un favor que cumple con las normas de conducta
+    //     Undefined. Implement with the following snippet:
+
+          When('publiqué un favor que cumple con las normas de conducta', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+
+    // ? Then mi publicación se mantendrá en el feed
+    //     Undefined. Implement with the following snippet:
+
+          Then('mi publicación se mantendrá en el feed', function () {       
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
